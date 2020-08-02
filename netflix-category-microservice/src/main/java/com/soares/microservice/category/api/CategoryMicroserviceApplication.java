f171a0cd016efb13432c05da8d047dd81ba5a651
Marcelo Soares <marceloh.web@gmail.com>
2020-08-02 10:58:31 -0300

    fix: removing unecessary annotations

--- netflix-category-microservice/src/main/java/com/soares/microservice/category/api/CategoryMicroserviceApplication.java
+++ netflix-category-microservice/src/main/java/com/soares/microservice/category/api/CategoryMicroserviceApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = "com.soares.microservice.commons.entity")
-@EnableJpaRepositories(basePackages = "com.soares.microservice.category.api.repository")
 public class CategoryMicroserviceApplication {
 
 	public static void main(String[] args) {
--- netflix-movie-microservice/src/main/java/com/soares/microservice/movie/api/MovieMicroserviceApplication.java
+++ netflix-movie-microservice/src/main/java/com/soares/microservice/movie/api/MovieMicroserviceApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = "com.soares.microservice.commons.entity")
-@EnableJpaRepositories(basePackages = "com.soares.microservice.movie.api.repository")
 public class MovieMicroserviceApplication {
 
 	public static void main(String[] args) {
