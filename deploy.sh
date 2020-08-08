c7a36729b332c5be1cfc6cacbd639c8b41cf04f8
Marcelo Soares <marceloh.web@gmail.com>
2020-08-08 19:31:29 -0300

    feat: removing deploy directory

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
 case $1 in
   "build")
 
-    echo "creating deploy directory"
-
-    [ ! -d "$(pwd)/netflix-config/deploy" ] && mkdir -p netflix-config/deploy ;
-    [ ! -d "$(pwd)/netflix-api-gateway/deploy" ] && mkdir -p netflix-api-gateway/deploy ;
-    [ ! -d "$(pwd)/netflix-service-discovery/deploy" ] && mkdir -p netflix-service-discovery/deploy ;
-    [ ! -d "$(pwd)/netflix-category-microservice/deploy" ] && mkdir -p netflix-category-microservice/deploy ;
-    [ ! -d "$(pwd)/netflix-movie-microservice/deploy" ] && mkdir -p netflix-movie-microservice/deploy ;
-    [ ! -d "$(pwd)/netflix-user-microservice/deploy" ] && mkdir -p netflix-user-microservice/deploy ;
-
     echo "creating compiler container"
 
     docker build . -t microservices-compiler
